4410bac872f291bd35b497f877c664a7881528ea
Aditi Sharma <aditisharma732006@gmail.com>
2026-05-13 18:23:51 +0530

    Initial commit

---
+++ appointment-manager/src/main/java/com/project/appointmentmanager/AppointmentManagerApplication.java
@@ -0,0 +1,13 @@
+package com.project.appointmentmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppointmentManagerApplication.class, args);
+	}
+
+}
